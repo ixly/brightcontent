89c2c11df6826c92c9f7aceee0dcdbfcfd710182
Egbert Veenstra <egbertveenstra@gmail.com>
2013-10-15 17:43:40 +0200

    ignore main.js

--- core/app/assets/javascripts/brightcontent/main.js
+++ core/app/assets/javascripts/brightcontent/main.js
@@ -1,28 +1,28 @@
-$(document).ready(function(){
-  $('textarea[name*="content"], textarea[name*="body"], textarea[name*="editor"]').each(function(i, elem) {
-    $(elem).wysihtml5({
-      html: true,
-      locale: "nl-NL",
-      parserRules:  wysihtml5ParserRules,
-      "events": {
-        "load": function() {
-          editor = $('#insertable').data("wysihtml5").editor;
-          editor.focus();
-        }
-      }
-    });
-  });
-
-  $("#attachments").on("click", ".insert_image", function(e){
-    e.preventDefault();
-    console.log($(this).data("insertImage"));
-    editor.composer.commands.exec("insertImage", { src: $(this).data("insertImage") });
-  });
-
-  $("#attachments").on("click", ".insert_link", function(e){
-    e.preventDefault();
-    console.log($(this).data());
-    editor.composer.commands.exec("createLink", { href: $(this).data("insertUrl"), target: "_blank", rel: "nofollow", text: $(this).data("insertName") });
-  });
-
-});
+// $(document).ready(function(){
+//   $('textarea[name*="content"], textarea[name*="body"], textarea[name*="editor"]').each(function(i, elem) {
+//     $(elem).wysihtml5({
+//       html: true,
+//       locale: "nl-NL",
+//       parserRules:  wysihtml5ParserRules,
+//       "events": {
+//         "load": function() {
+//           editor = $('#insertable').data("wysihtml5").editor;
+//           editor.focus();
+//         }
+//       }
+//     });
+//   });
+// 
+//   $("#attachments").on("click", ".insert_image", function(e){
+//     e.preventDefault();
+//     console.log($(this).data("insertImage"));
+//     editor.composer.commands.exec("insertImage", { src: $(this).data("insertImage") });
+//   });
+// 
+//   $("#attachments").on("click", ".insert_link", function(e){
+//     e.preventDefault();
+//     console.log($(this).data());
+//     editor.composer.commands.exec("createLink", { href: $(this).data("insertUrl"), target: "_blank", rel: "nofollow", text: $(this).data("insertName") });
+//   });
+// 
+// });
